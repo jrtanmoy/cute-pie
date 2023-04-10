fcef9a6299f1514131d4843b2f7f0f72109e9b6a
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-04-10 14:30:43 +0600

    header responsive done

--- assets/js/community/script.js
+++ assets/js/community/script.js
@@ -1,15 +1,26 @@
 // side nav start 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "180px";
+$(".sidenav_bar").click(function(){
+  $(".community-sidenav").toggleClass("main");
+  $(this).toggleClass('active')
+  if($(this).hasClass('active')){
+    $(this).find('i').addClass("fa-times").removeClass("fa-bars");
+    
+  }else{
+    $(this).find('i').addClass("fa-bars").removeClass("fa-times");
+
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+});
+
+
+// mobile menu 
+$(".menu-show").click(function(){
+  $(".menu_list").toggleClass("main");
+});
+
 
 document.querySelectorAll(".sidebar-list li a").forEach((link) => {
     if (link.href === window.location.href) {
         link.classList.add("side-active");
         link.setAttribute("aria-current", "page");
     }
-  });
+});
