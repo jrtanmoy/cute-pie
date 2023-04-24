316c772b0c0d72f8baf202f995e31cb877dc6a94
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-04-24 12:43:51 +0600

    js edit

--- assets/js/services/script.js
+++ assets/js/services/script.js
@@ -197,14 +197,4 @@ $('.').slick({
     // instead of a settings object
   ]
 });
-$(document).ready(function() {
-	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
 
-		fixedContentPos: false
-	});
-});
