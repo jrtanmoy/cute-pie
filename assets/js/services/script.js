01b1f3c6e48c17eb3fd4612e43768bc18d361cb7
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-05-04 12:06:33 +0600

    error fix

--- assets/js/services/script.js
+++ assets/js/services/script.js
@@ -163,50 +163,6 @@ $(".play-icon").modalVideo({
 });
 
 
-// $('.').slick({
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   autoplay: false,
-//   // autoplaySpeed: 1000,
-//   arrows: true,
-//   dots: false,
-//   nextArrow: "<button class='slick-prev pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
-//   prevArrow: "<button class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-//   responsive: [
-//     {
-//       breakpoint: 1024,
-//       settings: {
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: false
-//       }
-//     },
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 1
-//       }
-//     },
-//     {
-//       breakpoint: 576,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     }
-//     // You can unslick at a given breakpoint now by adding:
-//     // settings: "unslick"
-//     // instead of a settings object
-//   ]
-// });
-// $('.video-active').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3
-// });
-// for maps 
 
 $(document).ready(function(){
   $('.video-active').slick({
